2d5d0c808a6d55b2196a249f7013edf168c15af1
alanjefares <alan.jeffares.1@ucdconnect.ie>
2020-02-13 16:56:33 +0000

    ex 4.9, QDA on iris and add contours to LDA on iris

--- chapter-4/README.md
+++ chapter-4/README.md
@@ -6,7 +6,9 @@
 
 ## Code implementations
 * [LDA implementation using eqn 4.10](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/code/LDA.R) - My implementation Linear Discriminant Analysis and visualisation of the decision boundaries on the iris dataset. 
-<img src="https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/images/LDA_decision_boundaries.png"  width="400">
+<img src="https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/images/LDA_decision_boundary.png"  width="400"><img src="https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/images/LDA_decision_boundary_contour.png"  width="400">
+* [QDA implementation using eqn 4.12](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/code/LDA.R) - My implementation Quadratic Discriminant Analysis and visualisation of the decision boundaries on the iris dataset. 
+<img src="https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/images/QDA_decision_boundary.png"  width="400"><img src="https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/images/QDA_decision_boundary_contour.png"  width="400">
 
 ## Exercises
 * [Exercise 4.2 (a)](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/exercises/exercise_4.2_a.pdf) 
@@ -16,3 +18,4 @@
 * [Exercise 4.2 (e)](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/exercises/exercise_4.2_e.pdf)
 * [Exercise 4.3](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/exercises/exercise_4.3.pdf)
 * [Exercise 4.4](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/exercises/exercise_4.4.pdf)
+* [Exercise 4.9](https://github.com/alanjeffares/elements-of-statistical-learning/blob/master/chapter-4/code/QDA.R)
--- chapter-4/code/QDA.R
+++ chapter-4/code/QDA.R
@@ -1,14 +1,15 @@
+library(mvtnorm)
 library(datasets)
 
-# main logic for fitting LDA using discriminant function (eqn 4.10)
-LDA <- function(data, target){
+# main logic for fitting QDA using discriminant function (eqn 4.12) & "Computations for LDA" 4.3.2
+QDA <- function(data, target){
   unique(data[target])
   classes <- unique(data[,target])
   K <- length(classes)
   N <- nrow(data)
   pi_list <- vector('list', length=K)  # store k pi_k's
   mu_list <- vector('list', length=K)  # store k mu_k's
-  sigma_list <- vector('list', length=K)  # store k cov matrices to sum
+  sigma_list <- vector('list', length=K)  # store k sigma_k's
   
   # loop through classes to get parameter estimates by class
   # eg pi_k, mu_k and sigma_k
@@ -21,54 +22,61 @@ LDA <- function(data, target){
     X_subset <- subset_df[ , !names(subset_df) %in% c(target)]
     mu_k <- colSums(X_subset)/N_k  # p x 1 
     X_subset_cent <- sweep(X_subset, 2, mu_k, FUN = '-')
-    sigma_k <- cov(X_subset_cent)*(N_k-1)  # N_k-1 as cov function has this on the denominator
+    sigma_k <- cov(X_subset_cent)
     pi_list[[i]] <- pi_k
     mu_list[[i]] <- mu_k
     sigma_list[[i]] <- sigma_k
   }
-  sigma <- Reduce('+', sigma_list)/(N-K)
   # return estimates for pi, mu and sigma
-  output <- list(pi_list, mu_list, sigma)
-  names(output) <- list('pi_k', 'mu_k', 'sigma')
+  output <- list(pi_list, mu_list, sigma_list)
+  names(output) <- list('pi_k', 'mu_k', 'sigma_k')
   return(output)
 }
 
-
-predict_discriminants <- function(x, pi_list, mu_list, sigma, K=length(pi_list)){
+predict_discriminants <- function(x, pi_list, mu_list, sigma_list, K=length(pi_list)){
   scores <- c()
-  # apply eqn 4.10 for each of the K classes
+  # apply eqn 4.12 for each of the K classes
   for (i in 1:K){
-    a <- x %*% solve(sigma) %*% mu_list[[i]]
-    b <- 0.5*t(mu_list[[i]]) %*% solve(sigma) %*% mu_list[[i]]
-    score <- as.numeric(a - b + log(pi_list[[i]]))
+    sigma <- sigma_list[[i]]
+    mu <- mu_list[[i]]
+    pi <- pi_list[[i]]
+    
+    # see section 4.3.2 on p113 for details
+    sphered <- eigen(sigma)
+    U <- sphered$vectors
+    D <- diag(sphered$values)
+    
+    a <- t(t(U) %*% (t(x) - mu)) %*% solve(D) %*% t(U) %*% (t(x) - mu)
+    b <- sum(diag(log(D)))
+    
+    score <- as.numeric(-0.5*a - 0.5*b + log(pi))
     scores[[i]] <- score
   }
   return(scores)
 }
 
 # predict label for a single observation
-predict_label <- function(x, pi_list, mu_list, sigma){
-  preds <- predict_discriminants(x, pi_list, mu_list, sigma)
+predict_label <- function(x, pi_list, mu_list, sigma_list){
+  preds <- predict_discriminants(x, pi_list, mu_list, sigma_list)
   return(which.max(preds))
 }
 
 # predict for a full data.frame of observations
-predict_labels <- function(X, pi_list, mu_list, sigma){
+predict_labels <- function(X, pi_list, mu_list, sigma_list){
   predictions <- c()
   for (i in 1:nrow(X)){
     x <-  as.matrix(X[i,])
-    label_i = predict_label(x, pi_list, mu_list, sigma)
+    label_i = predict_label(x, pi_list, mu_list, sigma_list)
     predictions[i] <- label_i
   }
   return(predictions)
 }
 
 
-# now lets test out the fuction on a subset of the iris dataset
+# first fit to the data 
 data(iris)
 data_flat <- iris[,c(1,2,5)]
-# first fit to the data 
-fit <- LDA(data_flat, target = 'Species')
+fit <- QDA(data_flat, target = 'Species')
 
 # make a grid of values to predict on (so we can see the rough boundries)
 delta <- 0.05
@@ -77,50 +85,56 @@ y_vals = seq(from=min(data_flat$Sepal.Width)-delta, to=max(data_flat$Sepal.Width
 grid <- expand.grid(x=x_vals, y_vals)
 
 # predict the label of each point on the grid
-preds <- predict_labels(grid, fit$pi_k, fit$mu_k, fit$sigma)
+preds <- predict_labels(grid, fit$pi_k, fit$mu_k, fit$sigma_k)
 
 # visualise the results
 palette(c("skyblue","palegreen1","pink"))
-plot(data_flat$Sepal.Length, data_flat$Sepal.Width, xlab = 'Sepal Length', ylab = 'Sepal Width', type = 'n')
-title('LDA decision boundries on first 2 dimensions of Iris data', line = 0.4)
-points(x=grid[,1], y=grid[,2], col=preds, pch=20, cex=0.1)
+plot(x=data_flat$Sepal.Length, data_flat$Sepal.Width, xlab = 'Sepal Length', ylab = 'Sepal Width', type = 'n')
+title('QDA decision boundries on first 2 dimensions of Iris data', line = 0.4)
+points(x=grid[,1], y=grid[,2], col=preds, pch=20, cex=0.1)  # plot the colured grid in the background
 
 # optionally include contour plots
+levels <- list(c(0.5, 1, 1.5), c(0.5, 0.75, 1), c(0.5, 0.65, 0.8))  # custom contours
 nlevels <- c(3, 3, 2)
 x.points <- seq(4,8,length.out=100)
 y.points <- seq(1,5,length.out=100)
-sigma <- fit$sigma
 for (k in 1:length(fit$mu_k)){
   z <- matrix(0,nrow=100,ncol=100)
   mu <- fit$mu_k[[k]]
+  sigma <- fit$sigma_k[[k]]
   for (i in 1:100) {
     for (j in 1:100) {
       z[i,j] <- dmvnorm(c(x.points[i],y.points[j]),
                         mean=mu,sigma=sigma)
     }
   }
-  contour(x.points, y.points, z, add = TRUE, nlevels = 3)
+  contour(x.points, y.points, z, add = TRUE, levels = levels[[k]])
 }
 
 palette(c("blue","seagreen4","orangered3"))
-points(data_flat$Sepal.Length, data_flat$Sepal.Width, col=data_flat$Species, pch=16, cex=1.25)
+points(data_flat$Sepal.Length, data_flat$Sepal.Width, 
+       col=data_flat$Species, pch=16, cex=1.25)  # plot the true vals coloured by their actual label
+
+
 
 
 # testing performance on vowel data (https://web.stanford.edu/~hastie/ElemStatLearn/)
 test <- read.csv('/Users/alan.jeffares/Desktop/voweltest.csv')
 train <- read.csv('/Users/alan.jeffares/Desktop/voweltrain.csv')
 
-v_fit <- LDA(train[,2:12], target = 'y')
+v_fit <- QDA(train[,2:12], target = 'y')
 
 # confirm that these predictions match those in table 4.1 (p 107)
-train_preds <- predict_labels(train[,3:12], v_fit$pi_k, v_fit$mu_k, v_fit$sigma)
+train_preds <- predict_labels(train[,3:12], v_fit$pi_k, v_fit$mu_k, v_fit$sigma_k)
 train_accuracy <- sum(train_preds == train$y)/nrow(train)
 train_error <- 1 - train_accuracy
 print(paste0('Train error: ', train_error))
 
-test_preds <- predict_labels(test[,3:12], v_fit$pi_k, v_fit$mu_k, v_fit$sigma)
+test_preds <- predict_labels(test[,3:12], v_fit$pi_k, v_fit$mu_k, v_fit$sigma_k)
 test_accuracy <- sum(test_preds == test$y)/nrow(test)
 test_error <- 1 - test_accuracy
 print(paste0('Test error: ', test_error))
 
 # and they match!
+
+
