27ea28b08b30e0d65427a9f1f6409042a41d6d1b
alanjefares <alan.jeffares.1@ucdconnect.ie>
2020-03-23 13:52:00 +0000

    added code that failed to upload in previous push

--- chapter-5/code/periodic_splines.R
+++ chapter-5/code/periodic_splines.R
@@ -1,24 +1,33 @@
+# script to generate splines for periodic data as derived in question 5.6
+
+set.seed(11) 
+
 period <- 2*pi
 x = runif(100, 0, 2*period)
 y = sin(x) + rnorm(100, sd=0.5)
 
-
 # plot the data 
-plot(x,y, type = 'n', xaxt='n', yaxt='n')
 delta = 0.01
+plot(x, y, type = 'n', xlim = c(0.3, 2*period - 0.3), xaxt='n', yaxt='n', xlab = '', ylab = '')
+title(main = 'Cubic Splines for Periodic Data', cex.main = 1.2)
 rect(0, par("usr")[3]+delta, period/3, par("usr")[4]-delta,col = "gold1", border = NA)
 rect(period/3, par("usr")[3]+delta, 2*period/3, par("usr")[4]-delta,col = "gold3", border = NA)
 rect(2*period/3, par("usr")[3]+delta, period, par("usr")[4]-delta, col = "gold1", border = NA)
 rect(period, par("usr")[3]+delta, 4*period/3, par("usr")[4]-delta,col = "gold3", border = NA)
 rect(4*period/3, par("usr")[3]+delta, 5*period/3, par("usr")[4]-delta, col = "gold1", border = NA)
 rect(5*period/3, par("usr")[3]+delta, 2*period, par("usr")[4]-delta,col = "gold3", border = NA)
-axis(side = 1, at = c(0, period/3, 2*period/3, period, 4*period/3, 5*period/3, 2*period),
-     labels = c(0, expression(frac(2*T,3)), expression(xi[2]), 1,3,4, 6), cex.axis=1, line = +0.3, lwd = 0)
-points(x,y)
-?axis
+axis(side = 1, at = c(0, period, 2*period),
+     labels = c(0, expression(T), expression(2*T)), cex.axis=1, line = +0.3, lwd = 0)
+abline(v=period/3, lty=5)
+abline(v=2*period/3, lty=5)
+abline(v=period, lty=5)
+abline(v=4*period/3, lty=5)
+abline(v=5*period/3, lty=5)
+points(x,y, cex = 1.5)
+
 # plot the true underlying function
 x_vals = seq(from=0, to=2*period, length.out = 200)
-lines(x_vals, sin(x_vals), lwd=2, col='darkblue')
+lines(x_vals, sin(x_vals), lwd=7, col='darkblue')
 
 # subtract the period from observations until they are at their minimum positve value
 x <- ifelse(x>period, x-period, x)
@@ -30,16 +39,16 @@ mod_vals = seq(from=0, to=period, length.out = 100)
 mod_basis <- cbind(rep(1,100), mod_vals**2 - period*mod_vals,  mod_vals**3 - (period**2)*mod_vals)
 y_mod <- mod$coefficients[1]*mod_basis[,1] + mod$coefficients[2]*mod_basis[,2] +
   mod$coefficients[3]*mod_basis[,3]
-lines(mod_vals, y_mod, col='green')
-lines(mod_vals + period, y_mod, col='green')
+lines(mod_vals, y_mod, col='forestgreen', lwd = 4.5)
+lines(mod_vals + period, y_mod, col='forestgreen', lwd = 4.5)
 
-# first order continuous
+# first order continuous (NOT RUN)
 basis <- cbind(rep(1,100), -(5/2)*x*period**2 + (3/2)*period*x**2 + x**3)
 mod <- lm(y ~ 0 + basis[,1] + basis[,2])
 mod_vals = seq(from=0, to=period, length.out = 100)
 mod_basis <- cbind(rep(1,100), -(5/2)*mod_vals*period**2 + (3/2)*period*mod_vals**2 + mod_vals**3)
 y_mod <- mod$coefficients[1]*mod_basis[,1] + mod$coefficients[2]*mod_basis[,2] 
-lines(mod_vals, y_mod)
+#lines(mod_vals, y_mod)
 
 # fitting a spline with 2 knots
 positive_part <- function(x){x[x<0] <- 0; return(x)}
@@ -54,8 +63,9 @@ mod_basis <- cbind(rep(1,100), mod_vals**2 - period*mod_vals, mod_vals**3 - mod_
                    positive_part(mod_vals-2*period/3)**3 - mod_vals*((period - 2*period/3)**3)/period)
 y_mod <- mod$coefficients[1]*mod_basis[,1] + mod$coefficients[2]*mod_basis[,2] + mod$coefficients[3]*mod_basis[,3] +
    mod$coefficients[4]*mod_basis[,4] + mod$coefficients[5]*mod_basis[,5] 
-lines(mod_vals, y_mod, col='red')
-lines(mod_vals + period, y_mod, col='red')
-
-
+lines(mod_vals, y_mod, col='violetred3', lwd = 4.5)
+lines(mod_vals + period, y_mod, col='violetred3', lwd = 4.5)
+legend(x = 4.05*period/3, y = 2, 
+       legend = c('True function', 'Periodic global cubic polynomial', 'Periodic cubic polynomial spline'),
+       col = c('darkblue', 'forestgreen', 'violetred3'), lty = 1, lwd = 3, cex = 0.65) 
 
